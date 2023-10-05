3afa6d9d48829a565124e53243dc726e306a8b9b
skeliaa <chrisshimi9690@gmail.com>
2023-10-05 19:55:07 -0300

    ola

--- tarea1/main3.c
+++ tarea1/main3.c
@@ -14,7 +14,7 @@ struct charM {
     char** mp;
     int dimension;
 };
-// strReverse da vuelta la palabra si es que es necesario (si la palabra buscada en la sopa es nomaj, la da vuelta a jamon)
+
 char* strReverse(char* src){
     int l = strlen(src);
     char* dest = calloc(l,sizeof(char));
@@ -23,7 +23,14 @@ char* strReverse(char* src){
     }
     return dest;
 }
-// char* getline extrae un string correspondiente a una linea dentro de un archivo txt
+void liberar(struct charM* matriz){
+    for(int i = matriz->dimension; i>0; i--){
+        free(matriz->mp[i]);
+    }
+    free(matriz->mp);
+    free(matriz);
+}
+
 char* getLine(FILE* fp){
     char temp[MAX];
     int i = 0;
@@ -37,21 +44,74 @@ char* getLine(FILE* fp){
     }
     if(temp[i-1]<65 || temp[i-1]>122){
         temp[i-1] = 0;
-    } 
+    } // por alguna razón a veces se añadia el caracter 127 al final de forma ¿aleatoria?
+    //printf("%d\n",i);
     size_t u = i;
+    //printf("u1: %ld || ",u);
     char* line = calloc(u,sizeof(char));
-    strncpy(line,temp,u);
-    for(int j = 0;j<strlen(line+1);j++){
-        if (line[j]< 65 || line[j]> 90){
-            
-        }
-    }
-    
+    //strncpy(line,temp,u);
+    strcpy(line,temp);
+    //printf("u2: %ld || ",u);
+    //printf("len: %ld\n",strlen(line));
+    //printf("%s (in getLine de largo: %ld) (%d)\n",line,strlen(line),line[strlen(line)-1]);
     return line;
 }
 
+/* int comprobarSopa(char* clave, char* archivo){
+    //int flag = 1;
+    //char ** strArray;
+    char temp[MAX];
+    char temp2[MAX];
+    int c;
+    int i,u = 0;
+    int i2 = 2;
+    FILE *arc = fopen(archivo,"r");
 
-// struct charM* crearMatriz hace una matriz de la sopa de letras contenida en un archivo txt
+    if (arc != NULL){    
+        printf("Archivo abierto :D\n");
+        fgets(temp,__INT_MAX__,arc);
+        int largo = strlen(temp)/2;
+        printf("Largo de la sopa de letras: %d\n",largo);
+        char* strArray[largo];
+        strArray[i] = temp;
+        i++;
+        while (i<largo){
+            fgets(temp,__INT_MAX__,arc);
+            while(c != '\n' || c != ' '){
+                temp[u] = fgetc(arc);
+            }
+            strArray[i] = temp;
+            printf("%d: %s",i2,strArray[i]);
+            i++;
+            i2++;
+            //TODAS LAS LINEAS TIENEN SALTO DE LINEA 
+            //MENOS LA ULTIMA
+        }
+        //strcat(clave,"miau");
+        strcat(strArray[i-1],"\n"); //añadir el ulitmo salto de linea
+        printf("%s",strArray[i-1]);
+        fclose(arc);
+        printf("Archivo cerrado :D\n");
+
+        int j = 0;
+        int flag = 0;
+        while(!flag && j<=i){
+            if(strstr(strArray[j],clave)!=NULL){
+                flag = 1;
+            }
+            j++;
+        }
+        if (flag){printf("ENCONTRADOOOOO, lo encontre, lo encontre\n");}
+        else {printf("tu wea no está >:c\n");}
+        return 0;
+    }
+    else{return -1;}
+
+    //faltan dos cosas nomas: - (R)quitar espacios a la hora de getear lineas.
+    //                        -caso vertical -> trasponer el strArray.
+}
+ */
+
 struct charM* crearMatriz(const char* nombreArchivo){
     char* tmp;
     int i= 0;
@@ -62,40 +122,51 @@ struct charM* crearMatriz(const char* nombreArchivo){
         return NULL;
     }
     tmp = getLine(fp);
+    printf("orientaciooon: %s\n",tmp);
     strcpy(strArray->orientacion,tmp);
     tmp = getLine(fp);
     const int n = strlen(tmp);
+    printf("%d\n",n);
     strArray->mp = calloc(n,sizeof(char*));
     strArray->mp[0] = tmp;
-    strArray->dimension = strlen(tmp);
+    strArray->dimension = n;
+    //printf("%s (in getLine de largo: %d) (%d)\n",tmp,n,tmp[n-1]);
     i++;
     while(!feof(fp)){
+        //printf("a\n");
         strArray->mp[i] = getLine(fp);
+        //printf("Linea %d: %s\n",i+1,strArray->mp[i]);
         i++;
     }
+    int m = strlen(strArray->mp[1]);
+    if(strArray->mp[1][m-1]<65 || strArray->mp[1][m-1]>122){
+        strArray->mp[1][m-1] = 0;
+    }
+    //printf("Linea %d: %s\nchar n%d: %c\n",2,strArray->mp[1],m,strArray->mp[1][m-1]);
+    //sizeof();
     fclose(fp);
     return strArray;
 }
-// int buscarPalabra se dedica a buscar la palabra dentro de la matriz extraída de un archivo txt
 int buscarPalabra(struct charM *matrix, char* clave){
     for(int i = 0;i<matrix->dimension;i++){
         if(strstr(matrix->mp[i],clave) != NULL || strstr(matrix->mp[i],strReverse(clave)) != NULL){
+            printf("%d\n",i);
             return 1;
+            
         }
     }
     return 0;
 }
-//int trasponerMatriz da vuelta la matriz extraída de un archivo txt en caso de que la orientacion de este sea vertical
+
 int trasponerMatriz(struct charM* matrix){
+    printf("vamos a trasponer rawr\n");
     char tmp;
     int n = matrix->dimension;
-    if (n > 200){
-        n = 200;
-    }
-    
-    for(int i = 0;i<n;i++){
+    printf("n: %d\n",matrix->dimension);
+    //matrix->mp[0][0]='Z';
+    for(int i = 75;i<n;i++){
         for(int j = i; j<n;j++){
-            
+            //printf("%d,%d\n",i,j);
             tmp = matrix->mp[i][j];
             matrix->mp[i][j] = matrix->mp[j][i];
             matrix->mp[j][i] = tmp;
@@ -103,18 +174,27 @@ int trasponerMatriz(struct charM* matrix){
     }
     return 0;
 }
-//printMatriz muestra por pantalla la sopa de letras extraída de un archivo txt
 void printMatriz(struct charM* matrix){
     printf("%d\n",matrix->dimension);
     for(int i = 0;i<matrix->dimension;i++){
         printf("Linea %d: %s\n",i,matrix->mp[i]);
     }
 }
-
+void prueba(){
+    FILE *arc = fopen("archivo.txt","r");
+    char* linea;
+    if (arc != NULL){    
+        printf("Archivo abierto :D\n");
+        linea = getLine(arc);
+        printf("%s\n",linea);
+        free(linea);
+        return;
+    }
+}
 int carpetas(){
     DIR *dir;
     struct dirent* entry;
-    char direct[300] = "./archivos/";
+    char direct[100] = "./archivos/";
     char cambiar[300];
 
     const char *archiv = "./archivos";
@@ -230,8 +310,6 @@ int carpetas(){
     }
     return 0;
 }
-
-
 int main(){
     printf("Inicio de la ejecución\n");
     int opcion;
@@ -240,14 +318,14 @@ int main(){
     if (opcion == 1){
         carpetas();
     }else if(opcion == 2){
-        struct charM *matrix = crearMatriz("gato.txt");
+        struct charM *matrix = crearMatriz("hola.txt");
         printf("orientacion: %s\n",matrix->orientacion);
         if(strcmp(matrix->orientacion,"vertical") == 0){
             trasponerMatriz(matrix);
         }
         clock_t start = clock();        
         for (int i = 0; i<100;i++){
-            buscarPalabra(matrix,"GATO");
+            buscarPalabra(matrix,"HOLA");
         }
         clock_t end = clock();
         double total = (double)(end - start) / (CLOCKS_PER_SEC*100);
